29a064a191ea5c63a70040777a47f0fcec7facca
crays <cristhian.amaya13@gmail.com>
2021-02-08 21:04:14 -0400

    done

--- app.js
+++ app.js
@@ -17,31 +17,40 @@ app.get("/", (req, res)=>{
 
 
 const Telegraf = require('telegraf');
-
+const Extra = require('telegraf/extra');
  const bot = new Telegraf('1467528500:AAE1qHi8R97qsDfVdUQsyolZrz9QFgORSZ8');
 
- bot.start((ctx)=>{
-     ctx.reply('¿Donde está david? he venido a cogermelo')
+ bot.start( async (ctx)=>{
+    return await ctx.reply('¿Donde está david? he venido a cogermelo.');
  });
 
-bot.hears(['Walter marisco', 'walter marisco', 'Walter marico', 'walter marico', 'WALTER MARICO', 'WALTER MARISCO'], (ctx)=>{
-    ctx.reply(ctx.from.first_name +', dile a tu mamá por qué yo soy marisco');
+  bot.hears(['Walter marisco', 'walter marisco', 'Walter marico', 'walter marico', 'WALTER MARICO', 'WALTER MARISCO'], async (ctx)=>{
+    return  await ctx.reply(ctx.from.first_name +', dile a tu mamá por qué yo soy marisco...',  Extra.inReplyTo(ctx.message.message_id));
 });
 
-bot.hears(['Malditos frikis', 'malditos frikis', 'malditos frikis xd', 'Maldito friki'], (ctx)=>{
-    ctx.reply(ctx.from.first_name +', Mi pana vaya a doblarsela al jefe');
+bot.hears(['Malditos frikis', 'malditos frikis', 'malditos frikis xd', 'Maldito friki'], async (ctx)=>{
+    return   await ctx.reply(ctx.from.first_name +', Mi pana vaya a doblarsela a... Verga loco, David me dijo anoche que te botaron', Extra.inReplyTo(ctx.message.message_id));
 });
 
-bot.hears(['Mmlo', 'MMLO', 'mmlo', 'mmlo, anda', 'mmlo anda', 'MMlo', 'Mamenlo', 'mamenlo'], (ctx)=>{
-    ctx.reply('PERO PELATE ESA VERGA PUES PELATELO');
+bot.hears(['Mmlo', 'MMLO', 'mmlo', 'mmlo, anda', 'mmlo anda', 'MMlo', 'Mamenlo', 'mamenlo', 'Mamalo', 'mamalo', 'MAMALO'], async (ctx)=>{
+    return  await ctx.reply('PERO PELATE ESA VERGA PUES, PELATELA.',  Extra.inReplyTo(ctx.message.message_id));
 });
 
-bot.mention(['Delpretti', 'OnlyCrays', 'PicoTresxd', 'JesusFIR'], (ctx)=>{
-    ctx.reply(ctx.from.first_name +', Mmlo');
+bot.mention(['Delpretti', 'OnlyCrays', 'PicoTresxd', 'JesusFIR'],  async(ctx)=>{
+    return  await  ctx.reply(ctx.from.first_name +', Mmlo.',  Extra.inReplyTo(ctx.message.message_id));
 });
 
-bot.hears(['piwi', 'PIWI', 'Piwi'],(ctx)=>{
-    ctx.reply(ctx.from.first_name +', Mmlo');
-})
+bot.hears(['piwi', 'PIWI', 'Piwi'], async (ctx)=>{
+    return  await ctx.reply(ctx.from.first_name +', Mmlo.',  Extra.inReplyTo(ctx.message.message_id));
+});
+bot.hears(['F', 'F, bro', 'f', 'f, bro', 'F, mano', 'f, mano', 'coño, f', 'Coño, F'], async (ctx)=>{
+    return  await ctx.reply('Coño mano, F.',  Extra.inReplyTo(ctx.message.message_id));
+});
+ bot.command('send', async (ctx)=>{
+    return await ctx.reply('@Delpretti te envié un correo, revisa el OutLook.');
+});
+bot.hears(['Verga','VERGA', 'verga', 'VERGA...', 'Verga...', 'verga...', 'VErga' ], async (ctx)=>{
+    return  await ctx.reply('Exacto...',  Extra.inReplyTo(ctx.message.message_id));
+});
 
  bot.launch()
